b195dc6a838ecde6b1e8cf8472f573a1d3ee5408
RaimundoSilva03 <126274796+RaimundoSilva03@users.noreply.github.com>
2024-11-20 23:05:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
-int main(){
+typedef struct {
+    char nome[80];
+    char isbn[20];
+    float preco;
+    int score;
+    char editora[20];
+} livro;
 
-    //implemente seu codigo
-    printf("Nao implementado ainda.");
+typedef livro *pLivro;
+//Função pra alocar a memória
+  pLivro livro_aloc(int qtde) {
     
-    return 0;
+    return (pLivro)malloc(qtde * sizeof(livro));
 }
+
+//Função pra ler os dados
+void livro_ler(pLivro livros, int qtde) {
+	int i;
+    for(i = 0; i < qtde; i++) {
+    	
+        getchar();
+        fgets(livros[i].nome, 80, stdin);
+        fflush(stdin);
+        
+        fgets(livros[i].isbn, 20, stdin);
+        fflush(stdin);
+        
+        scanf("%f", &livros[i].preco);
+        
+        scanf("%i", &livros[i].score);
+        
+        getchar();
+        fgets(livros[i].editora, 20, stdin); 
+        
+    }
+    
+}
+//Função para exibir os dados
+void livro_exibe(pLivro livros, int qtde) {
+	int i;
+    for(i = 0; i < qtde; i++) {
+        printf("\nLivro %d: ", i+1);
+        printf("\n\nNome: %s\n", livros[i].nome);
+        printf("ISBN: %s\n", livros[i].isbn);
+        printf("Preço: R$ %.2f\n", livros[i].preco);
+        printf("\nScore: %d\n", livros[i].score);
+        printf("\nEditora: %s", livros[i].editora);
+        
+    }
+    
+}
+void livro_desaloca(pLivro livros) {
+	free(livros);
+}
+
+int main()
+{
+	
+	setlocale(LC_ALL, "portuguese");
+	setlocale(LC_NUMERIC, "C");
+    int qtde;
+    scanf("%i", &qtde);
+    
+    pLivro livros = livro_aloc(qtde);
+    
+    livro_ler(livros, qtde);
+    
+    livro_exibe(livros, qtde);
+    
+    livro_desaloca(livros);
+
+    return 0;
+} 
