757d449d4ee44299a4e3b0c58de9b5253bbfbc01
RaimundoSilva03 <126274796+RaimundoSilva03@users.noreply.github.com>
2024-11-24 23:02:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <ctype.h>
 
 typedef struct {
     char nome[80];
@@ -17,14 +18,12 @@ typedef livro* pLivro;
     
     return (pLivro)malloc(qtde * sizeof(livro));
 }
-
-//Função pra ler os dados
+//Função pra recber os dados
 void livro_ler(pLivro livros, int qtde) {
 	int i;
-	//printf("Dados do %dº livro: \n\n", i+1);
+	getchar();
     for(i = 0; i < qtde; i++) {
-    	
-        getchar();
+        
         fgets(livros[i].nome, 80, stdin);
         fflush(stdin);
         
@@ -37,7 +36,8 @@ void livro_ler(pLivro livros, int qtde) {
         
         getchar();
         fgets(livros[i].editora, 20, stdin); 
-        
+        fflush(stdin);
+        printf("\n");
     }
     
 }
@@ -45,27 +45,25 @@ void livro_ler(pLivro livros, int qtde) {
 void livro_exibe(pLivro livros, int qtde) {
 	int i;
     for(i = 0; i < qtde; i++) {
-        printf("\nLivro %d:\n", i+1);
-        printf("Nome: %s", livros[i].nome);
+        printf("Livro %d: ", i+1);
+        printf("\nNome: %s", livros[i].nome);
         printf("ISBN: %s", livros[i].isbn);
         printf("Preço: R$ %.2f\n", livros[i].preco);
         printf("Score: %d\n", livros[i].score);
         printf("Editora: %s", livros[i].editora);
-        
+        printf("\n");
     }
     
 }
+//Função para liberar a memória
 void livro_desaloca(pLivro livros) {
 	free(livros);
 }
 
 int main()
 {
-	
-	setlocale(LC_ALL, "portuguese");
-	setlocale(LC_NUMERIC, "C");
+	setlocale(LC_ALL, "pt_BR.UTF-8");
     int qtde;
-    //printf("Quantidade de livros: ");
     scanf("%i", &qtde);
     
     pLivro livros = livro_aloc(qtde);
@@ -78,3 +76,7 @@ int main()
 
     return 0;
 } 
+	
+	
+	
+	
